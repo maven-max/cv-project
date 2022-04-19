e5e478de15afa998ecbed9ec5e03ccca5d1e4977
maven-max <emmanuelegote@gmail.com>
2022-04-19 01:27:55 +0000

    changes in pracExp

--- src/components/pracExp.js
+++ src/components/pracExp.js
@@ -2,7 +2,7 @@ function PracExp() {
   return (
     <div>
       <h2>Practical Experience</h2>
-      <p>KATH</p>
+      <p>Komfo Anokye Teaching Hospital</p>
       <p>Intern</p>
       <p>main tasks</p>
       <p>Sep 2022 to Nov 2022</p>
